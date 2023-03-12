636f0646d35243e5bcbd431b95ccec3102531852
Maitreyi Rajaram <mrajaram@integralads.com>
2023-03-12 17:10:10 -0400

    Add workflow test

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 from databricks import sql
 
 
-
 def main():
     """
     Testing dynamic workflows on Databricks
@@ -12,7 +11,7 @@ def main():
     config = json.loads(open("config.json").read())
     with sql.connect(server_hostname="dbc-f7060552-825c.cloud.databricks.com",
                      http_path="/sql/1.0/warehouses/7f5f47a2f194c89d",
-                     access_token=os.getenv("DATABRICKS_TOKEN")) as connection:
+                     access_token=dbutils.secrets.get(scope="workflow", key="databricks_token")) as connection:
         with connection.cursor() as cursor:
             cursor.execute(
                 f'SELECT COUNT(DISTINCT station_name) FROM {config.get("catalog_name")}.{config.get("schema_name")}.{config.get("table_name")} '
@@ -21,8 +20,6 @@ def main():
             result = cursor.fetchall()
     for row in result:
         print(row)
-
-
 
 
 if __name__ == '__main__':
